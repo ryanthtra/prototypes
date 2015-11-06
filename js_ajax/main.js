602f42618cb3a1a969beb2120c375b07bb17a84f
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-11-05 17:36:39 -0800

    js_ajax - Ryan Transfiguracion

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result = null;
 
 $(document).ready(function(){
 	$('button').click(function(){
@@ -10,6 +10,24 @@ $(document).ready(function(){
 			success: function(result) {
 				console.log('AJAX Success function called, with the following result:', result);
 
+				// Feature Set #2
+				global_result = result;
+				var movie_info = global_result.feed.entry[0];
+				global_result = movie_info["im:image"][2]['label'];
+
+				// Feature Set #3
+				var movie_entries = result.feed.entry;
+				for (var i = 0; i < movie_entries.length; i++)
+				{
+					var new_image = $('<img>').attr('src', movie_entries[i]["im:image"][2]['label']);
+
+					// Feature Set #4
+					var name = $('<p>').text(movie_entries[i]["im:name"]["label"]);
+					var director = $('<p>').text('Directed by: ' + movie_entries[i]["im:artist"]["label"]);
+					var movie_span = $('<div>').append(new_image, name, director, $('<hr>'));
+
+					$('#main').append(movie_span);
+				}
 			}
 		});
 		console.log('End of click function');
