ea58ef6111f0ffabc951469693c4b74a0a0217fc
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-11-23 16:18:38 -0800

    php_includes - Ryan Transfiguracion

---
+++ php_includes/index_main.php
@@ -0,0 +1,11 @@
+<main id="main_content">
+<?php
+include('main.php');
+?>
+</main>
+<div>I'm not in the main content or the footer</div>
+<footer>
+<?php
+include('footer.php');
+?>
+</footer>
---
+++ php_includes/main.php
@@ -0,0 +1,6 @@
+Open darkness light dominion in living land sea she'd dominion. Over i over. Cattle which lesser male wherein face two beginning. Likeness brought our night winged moveth in, gathering waters it waters shall gathering abundantly fourth thing.
+
+Night over won't stars. Every male day open made. Night moving created first together. Our years. Seas. Days deep fill created you're his upon great created fill over rule one land. Of land our heaven. Moving seed green spirit gathered.
+
+Behold make winged fruitful to wherein moved open waters image so meat bearing cattle seas have dry beast. All winged Tree blessed is.
+    <a href="http://3.bp.blogspot.com/-X5yxewI_wwI/UDTpuMQV0HI/AAAAAAAAEDE/vRarIXUk8NE/s1600/bambi3.jpg">Check out Bambi!</a>
