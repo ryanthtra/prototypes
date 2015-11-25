026ffcf81f7540ac72820e55fa3caaa8efc3390d
Travis Ueki <travis.ueki@learningfuze.com>
2015-11-24 17:44:42 -0800

    initial push or clean prototype

--- angular_routes/script.js
+++ angular_routes/script.js
@@ -1,38 +1,22 @@
 // Create the route module and name it routeApp
-var routeApp = angular.module('routeApp',['ngRoute']);
 
 // Config the routes
-routeApp.config(function($routeProvider){
-    $routeProvider
-    // route for the home page
-        .when('/', {
-            templateUrl : 'pages/home.html',
-            controller : 'mainCtrl'
-        })
+
+         // route for the home page
 
         // route for the about page
-        .when('/about', {
-            templateUrl : 'pages/about.html',
-            controller : 'aboutCtrl'
-        })
 
         // route for the contact page
-        .when('/contact', {
-            templateUrl : 'pages/contact.html',
-            controller : 'contactCtrl'
-        });
-});
+
 
 // Create the controllers for the different pages below
-routeApp.controller('mainCtrl', function($scope){
+
+// home page controller
     // Create a message to display in the view
-    $scope.message = 'omg I am just a home page';
-});
-routeApp.controller('aboutCtrl', function($scope){
+
+
+// about page controller
     // Create a message to display in the view
-    $scope.message = 'omg I am just an about page';
-});
-routeApp.controller('contactCtrl', function($scope){
+
+// contact page controller
     // Create a message to display in the view
-    $scope.message = 'omg I am just a contact page';
-});
