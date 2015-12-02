dd02c685e8e1b672fb3ae36d577be75851eb273a
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-12-01 19:18:00 -0800

    php-sessions, Ryan Transfiguracion

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -1,1 +1,28 @@
-<!-- Session Reader -->
+<?
+session_start();
+date_default_timezone_set('utc');
+
+foreach ($_POST as $field=>$entry)
+{
+	$_SESSION[$field] = $entry;
+
+	if (preg_match('/^.{2,}$/', $entry))	// If this is a valid entry
+	{
+		// Remove the previous error message from the errors array, if there was one.
+		if (isset($_SESSION['errors'][$field]))
+			unset($_SESSION['errors'][$field]);
+	}
+	else // This is an invalid entry.
+	{
+		// Add an error message to the errors array.
+		$_SESSION['errors'][$field] = 'Invalid ' . $field;
+		header('location: session_setter.php');
+	}
+	echo($field . ': ' . $entry . '<br/>');
+}
+// If there were no errors, then just remove the errors array altogether.
+if (isset($_SESSION['errors']) && count($_SESSION['errors']) == 0)
+	unset($_SESSION['errors']);
+
+print('<a href="session_setter.php">Go back to form</a>');
+?>
--- php-sessions/session_setter.php
+++ php-sessions/session_setter.php
@@ -1,1 +1,15 @@
-<!-- Session Setter -->
+<?
+session_start();
+date_default_timezone_set('utc');
+?>
+<form action="session_reader.php" method="post">
+    <input type="text" name="name" placeholder="Name" value="<?if (isset($_SESSION['name'])) echo $_SESSION['name'];?>" /><br />
+	<p style="color: red"><?if (isset($_SESSION['errors']['name'])) echo $_SESSION['errors']['name'];?></p>
+    <input type="text" name="age" placeholder="Age" value="<?if (isset($_SESSION['age'])) echo $_SESSION['age'];?>" /><br />
+    <p style="color: red"><?if (isset($_SESSION['errors']['age'])) echo $_SESSION['errors']['age'];?></p>
+    <input type="text" name="occupation" placeholder="Occupation" value="<?if (isset($_SESSION['occupation'])) echo $_SESSION['occupation'];?>" /><br />
+    <p style="color: red"><?if (isset($_SESSION['errors']['occupation'])) echo $_SESSION['errors']['occupation'];?></p>
+	<button type="submit">Submit</button>
+	<br />
+</form>
+<?//print_r($_SESSION);?>
