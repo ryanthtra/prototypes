61c6e9d6985bb1e3bca69425e7ff300f64c97e25
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-12-03 23:17:14 -0800

    mysql-basics - Ryan Transfiguracion

--- mysql-basics/mysql_queries.sql
+++ mysql-basics/mysql_queries.sql
@@ -1,1 +1,6 @@
 Place your queries below
+SELECT * FROM `users` WHERE `username`='Ryan';
+UPDATE `users` SET `email`='ryanthtra@outlook.com' WHERE `username`='Ryan';
+INSERT INTO `users` SET `username`='Ursula', `email`='ihateariel@gmail.com', `password`=sha1('trytocrackthis');
+DELETE FROM `users` WHERE `username`='Ursula';
+
